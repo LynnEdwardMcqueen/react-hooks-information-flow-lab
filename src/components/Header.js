b02acbbfd8cbb4620d22b9558241a3a6eb448896
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-05-29 07:53:20 -0600

    Refactors Header out of App.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+
+function Header({darkMode, onDarkModeClick}) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {darkMode ? "Dark" : "Light"} Mode
+        </button>
+    </header>
+  )
+}
+
+export default Header
